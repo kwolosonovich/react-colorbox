13659b4c44acb42262cc7bb63fdb731f5db2e70f
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-11-08 19:30:09 -0700

    box list css file

--- src/BoxList.js
+++ src/BoxList.js
@@ -1,9 +1,8 @@
-import { defaultCipherList } from 'constants';
-import { uniqueId } from 'lodash';
 import React, {useState} from 'react';
 import {v4 as uuid} from "uuid";
 import Box from './Box'
 import NewBoxForm from './NewBoxForm'
+import './BoxList.css'
 
 const BoxList = () => {
     // initial state
